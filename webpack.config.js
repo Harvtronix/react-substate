e432243d060920bd4b874432aeebc10ff4910560
Harvtronix Webmaster <webmaster@harvtronix.com>
2020-04-20 00:16:49 -0400

    feat(build): minified + source map

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    devtool: 'cheap-module-source-map',
+    mode: 'production',
     entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
