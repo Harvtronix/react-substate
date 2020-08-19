d1057277f2b9344dde87b50d57526b6b93a3f017
Harvtronix Webmaster <webmaster@harvtronix.com>
2020-08-19 11:52:55 -0400

    fixes #5
    - usePatchEffect can now take either a single substate key or an array of substate keys

--- src/hooks/usePatchEffect.ts
+++ src/hooks/usePatchEffect.ts
@@ -10,33 +10,58 @@ import SubstateManager from '../managers/SubstateManager'
  *
  * @param {PatchEffectFunction} effectFunction Function to run each time patches are generated. This
  * function will receive the patches as an argument.
- * @param {keyof Substates} [substateKey] Optional substate key to limit which patches are passed
+ * @param {keyof Substates} [substateKeys] Optional substate key to limit which patches are passed
  * to this patch effect.
  */
-function usePatchEffect (effectFunction: PatchEffectFunction, substateKey?: keyof Substates): void {
-    if (substateKey !== undefined && !SubstateManager.hasSubstate(substateKey)) {
-        throw new Error('No substate found with key ' + substateKey)
+function usePatchEffect (
+    effectFunction: PatchEffectFunction,
+    substateKeys?: keyof Substates | Array<keyof Substates>
+): void {
+    const keyList: Array<keyof Substates> = []
+
+    if (substateKeys !== undefined) {
+        if (Array.isArray(substateKeys)) {
+            keyList.splice(0, 0, ...substateKeys)
+        } else {
+            keyList.push(substateKeys)
+        }
     }
 
-    useEffect(() => {
-        if (substateKey !== undefined) {
-            Debug.log('Registering patch effect for ' + substateKey)
-        } else {
-            Debug.log('Registering global patch effect')
+    keyList.forEach((substateKey) => {
+        if (!SubstateManager.hasSubstate(substateKey)) {
+            throw new Error('No substate found with key ' + substateKey)
         }
+    })
 
-        PatchManager.registerPatchEffect(effectFunction, substateKey)
+    useEffect(() => {
+        if (keyList.length === 0) {
+            Debug.log('Registering global patch effect')
+
+            PatchManager.registerPatchEffect(effectFunction)
+        } else {
+            // Register the effect function for each key
+            keyList.forEach((substateKey) => {
+                Debug.log('Registering patch effect for ' + substateKey)
+
+                PatchManager.registerPatchEffect(effectFunction, substateKey)
+            })
+        }
 
         return () => {
-            if (substateKey !== undefined) {
-                Debug.log('Unregistering patch effect for ' + substateKey)
-            } else {
+            if (keyList.length === 0) {
                 Debug.log('Unregistering global patch effect')
-            }
 
-            PatchManager.unregisterPatchEffect(effectFunction, substateKey)
+                PatchManager.unregisterPatchEffect(effectFunction)
+            } else {
+                // Unregister the effect function for each key
+                keyList.forEach((substateKey) => {
+                    Debug.log('Unregistering patch effect for ' + substateKey)
+
+                    PatchManager.unregisterPatchEffect(effectFunction, substateKey)
+                })
+            }
         }
-    }, [effectFunction, substateKey])
+    }, [effectFunction, substateKeys])
 }
 
 export default usePatchEffect
