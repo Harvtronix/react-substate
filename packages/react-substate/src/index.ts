93fecb5120c7d338c839124bffbf4a8419ae6ac4
Harvtronix <webmaster@harvtronix.com>
2024-09-18 00:39:37 -0400

    fix: export global dispatcher type

--- packages/react-substate/src/index.ts
+++ packages/react-substate/src/index.ts
@@ -3,7 +3,7 @@ import {useDispatch} from './hooks/useDispatch.js'
 import {useGlobalDispatch} from './hooks/useGlobalDispatch.js'
 import {usePatchEffect} from './hooks/usePatchEffect.js'
 import {useSubstate} from './hooks/useSubstate.js'
-import {Dispatcher} from './Interfaces.js'
+import {Dispatcher, GlobalDispatcher} from './Interfaces.js'
 import {createAction} from './managers/ActionManager.js'
 import {setDevToolsEnabled} from './managers/DevToolsManager.js'
 import {createSubstate} from './managers/SubstateManager.js'
@@ -31,4 +31,7 @@ export {
   useSubstate
 }
 
-export type {Dispatcher}
+export type {
+  Dispatcher,
+  GlobalDispatcher
+}
