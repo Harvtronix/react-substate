3478b1e3b1ed8f13c143c8a8020315e9c2d4f04e
Harvtronix <webmaster@harvtronix.com>
2024-11-24 21:15:28 -0500

    feat: enable access to immer config functions

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,3 +1,5 @@
+import { enableMapSet, enablePatches, setAutoFreeze, setUseStrictShallowCopy } from 'immer'
+
 import { setDebugEnabled } from './Debug.js'
 import { useDispatch } from './hooks/useDispatch.js'
 import { useSubstate } from './hooks/useSubstate.js'
@@ -6,18 +8,33 @@ import { createAction } from './managers/ActionManager.js'
 import { setDevToolsEnabled } from './managers/DevToolsManager.js'
 import { createSubstate } from './managers/SubstateManager.js'
 
+// Pre-configure Immer to opt out of auto-freezing. This can be overridden by downstream users.
+setAutoFreeze(false)
+
+/**
+ * Functions that allow additional configuration of the underlying Immer engine.
+ */
+const Immer = {
+  enableMapSet,
+  enablePatches,
+  setAutoFreeze,
+  setUseStrictShallowCopy
+}
+
 export default {
   createAction,
   createSubstate,
   setDebugEnabled,
   setDevToolsEnabled,
   useDispatch,
-  useSubstate
+  useSubstate,
+  Immer
 }
 
 export {
   createAction,
   createSubstate,
+  Immer,
   setDebugEnabled,
   setDevToolsEnabled,
   useDispatch,
