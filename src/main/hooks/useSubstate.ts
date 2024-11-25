58557cd5585e4b9bd27f31687a357ab77eb5c128
Harvtronix <webmaster@harvtronix.com>
2024-11-24 20:28:55 -0500

    docs: improved useSubstate docs

--- src/main/hooks/useSubstate.ts
+++ src/main/hooks/useSubstate.ts
@@ -5,8 +5,10 @@ import { Dispatcher, SubstateKey } from '../Interfaces.js'
 import { getSubstate, registerListener, unregisterListener } from '../managers/SubstateManager.js'
 
 /**
- * Hook that allows a component to listen for changes to a substate and receive a reference to a
- * dispatch function that can be called to update that substate.
+ * Hook that does the following things:
+ * - Provides the current value of the Substate
+ * - Allows a component to listen for changes to the Substate
+ * - Provides a Substate-specific dispatch function that can be called to updated the Substate
  * @param substateKey The key of the substate to return. The returned dispatch function will be
  * scoped to this substate as well.
  * @returns Object containing the current value of the Substate and a dispatch function that can be
